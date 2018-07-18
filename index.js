6e87ac2f128c38d269b9ef84ef471c2cd91879ba
sanath-kumar <sanathkumar790@gmail.com>
2018-07-18 20:16:34 +0530

    Updates

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express  = require('express')
-
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const socket = require('socket.io')
+const cors = require('cors')
 const keys = require('./config/keys')
-var cors = require('cors')
-
 const message = require('./model/message')
 
+const app = new express()
+app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.connect(keys.mongoURI);
 mongoose.connection.on('error',()=>{
@@ -17,10 +18,6 @@ mongoose.connection.once('open',function(){
   console.log("DB connection established")
 })
 
-const app = new express()
-
-app.use(bodyParser.json())
-app.use(cors())
 
 app.get('/chat', async (req,res) => {
   let result = await message.find()
@@ -35,11 +32,9 @@ let io =  socket(server);
 
 io.on("connection", function(socket){
   console.log("Socket Connection Established with ID :"+ socket.id)
-
   socket.on("chat", async function(chat){
     chat.created = new Date()
     let response = await new message(chat).save()
     socket.emit("chat",chat)
   })
-
 })
